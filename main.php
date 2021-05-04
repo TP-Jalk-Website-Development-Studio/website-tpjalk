9bb09e7acd398965660a7bdfb075d183bf59b202
vlkradek <radekvlk31@gmail.com>
2021-05-04 18:21:54 +0200

    h
    test

--- main.php
+++ main.php
@@ -139,43 +139,41 @@ body,h1,h2,h3,h4,h5 {font-family: "Raleway", sans-serif}
       </div>
       <hr class="w3-opacity">
       <h4 class="w3-padding-16 section-title">How much we charge</h4>
-      <div class="w3-row-padding" style="margin:0 -16px">
-        <div class="w3-half w3-margin-bottom">
-          <ul class="w3-ul w3-white w3-center w3-opacity w3-hover-opacity-off">
-            <li class="w3-black w3-xlarge w3-padding-32">Basic</li>
-            <li class="w3-padding-16">Web Coding</li>
-            <li class="w3-padding-16">Custom Design</li>
-            <li class="w3-padding-16">up to 3GB off files</li>
-            <li class="w3-padding-16">---</li>
-            <li class="w3-padding-16">
-              <h2>$ 20</h2>
-              <span class="w3-opacity">per one page</span>
-            </li>
-            <li class="w3-light-grey w3-padding-24">
-              <button class="w3-button w3-white w3-padding-large">Sign Up</button>
-            </li>
-          </ul>
+        <div class="w3-row-padding" style="margin:0 -16px">
+            <div class="w3-half w3-margin-bottom">
+                <ul class="w3-ul w3-white w3-center w3-opacity w3-hover-opacity-off">
+                    <li class="w3-black w3-xlarge w3-padding-32">Basic</li>
+                    <li class="w3-padding-16">Web Coding</li>
+                    <li class="w3-padding-16">Custom Design</li>
+                    <li class="w3-padding-16">up to 3GB off files</li>
+                    <li class="w3-padding-16">---</li>
+                    <li class="w3-padding-16">
+                        <h2>$ 20</h2>
+                        <span class="w3-opacity">per one page</span>
+                    </li>
+                    <li class="w3-light-grey w3-padding-24">
+                        <button class="w3-button w3-white w3-padding-large">Sign Up</button>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="w3-half">
+                <ul class="w3-ul w3-white w3-center w3-opacity w3-hover-opacity-off">
+                    <li class="w3-black w3-xlarge w3-padding-32">Pro</li>
+                    <li class="w3-padding-16">Web Coding</li>
+                    <li class="w3-padding-16">Custom Design</li>
+                    <li class="w3-padding-16">up to 7GB off files</li>
+                    <li class="w3-padding-16">24/7 support</li>
+                    <li class="w3-padding-16">
+                        <h2>$ 35</h2>
+                        <span class="w3-opacity">per one page</span>
+                    </li>
+                    <li class="w3-light-grey w3-padding-24">
+                        <button class="w3-button w3-white w3-padding-large">Sign Up</button>
+                    </li>
+                </ul>
+            </div>
         </div>
-        
-        <div class="w3-half">
-          <ul class="w3-ul w3-white w3-center w3-opacity w3-hover-opacity-off">
-            <li class="w3-black w3-xlarge w3-padding-32">Pro</li>
-            <li class="w3-padding-16">Web Coding</li>
-            <li class="w3-padding-16">Custom Design</li>
-            <li class="w3-padding-16">up to 7GB off files</li>
-            <li class="w3-padding-16">24/7 support</li>
-            <li class="w3-padding-16">
-              <h2>$ 35</h2>
-              <span class="w3-opacity">per one page</span>
-            </li>
-            <li class="w3-light-grey w3-padding-24">
-              <button class="w3-button w3-white w3-padding-large">Sign Up</button>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  </div>
 
   <!-- Contact section -->
   <div class="w3-container w3-light-grey w3-padding-32 w3-padding-large" id="contact">
